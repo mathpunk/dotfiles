94f9393b7cf575c2fc3afe973b0af27987226e10
Thomas H <tom@mathpunk.net>
2020-12-25 16:52:02 -0600

    Supposedly safe fish changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,29 @@
-# config
+# GOAL: Platform independent configuration. This might be the wrong abstraction layer but let's try it
+
+function is-mac
+    uname -a|bb -i '(boolean (re-find #"Darwin" (first *input*)))'
+end
+
+if is-mac
+    set OS "MacOS"
+else
+    set OS "Unknown"
+end
+
+function is-work-machine
+    is-mac
+end
+
+# Defaults
+# --------
 set fish_greeting ""
 fish_vi_key_bindings
 set -Ux EDITOR 'emacsclient -a "" -c' # this might work funny with no x server
 
-# editor
+
+# Abbreviations
+# -------------
 alias e='emacsclient -a "" -c'
-# alias ec="/usr/bin/emacsclient -ct"
-# alias es="/usr/bin/emacs --daemon"
 
 # git
 abbr gs "git status"
@@ -19,6 +36,7 @@ abbr gco "git checkout"
 abbr gcb "git checkout -b"
 abbr gl "git log --oneline --decorate --graph"
 
+# yadm
 abbr ys "yadm status"
 abbr ya "yadm add"
 abbr yc "yadm commit"
@@ -29,14 +47,7 @@ abbr yt "youtube-dl"
 
 # Path
 # =======
-# set PATH $PATH $HOME/.nvm/versions/node/v10.10.0/bin/
-# export NODE_OPTIONS="--max-old-space-size=3072" # lg
-# set PATH $PATH ~/system/bin
-# set PATH $PATH ~/.local/bin
-
-set PATH /Users/tomhenderson/bin /Users/tomhenderson/.nvm/versions/node/v10.10.0/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/opt/neo4j/bin /Users/tomhenderson/.nvm/versions/node/v10.10.0/lib/node_modules
-
-# /opt/leiningen /opt/arduino /Users/tomhenderson/.cargo/bin
+set PATH $HOME/bin /$HOME/.nvm/versions/node/v10.10.0/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/opt/neo4j/bin /$HOME/.nvm/versions/node/v10.10.0/lib/node_modules
 
 
 # NVM
@@ -49,20 +60,6 @@ nvm use default --silent
 
 # Neo4j
 export NEO4J_HOME=/usr/local/opt/neo4j
-# set PATH $PATH /usr/local/opt/neo4j/bin
-
-# Leiningen
-# set PATH $PATH /opt/leiningen
-
-# Hardware
-# set PATH $PATH /opt/arduino
-
-# Music Creation
-# export SOUND_FONT=/usr/share/soundfonts/FluidR3_GM.sf2
-
-# Rust Binaries
-# set PATH $PATH $HOME/.cargo/bin
-
 
 # Protractor
 abbr pro "./node_modules/protractor/bin/protractor e2e/conf.js "
@@ -75,13 +72,13 @@ alias dockerrmv="docker volume rm (docker volume ls -qf dangling=true)"
 
 # direnv
 eval (direnv hook fish)
-
 export JAVA_HOME=(/usr/libexec/java_home)
 
 # SECRETS / ENV
 # =================================
 source ~/.secrets.sh
 ssh-add ~/.ssh/id_logicgate
+ssh-add ~/.ssh/id_mathpunk
 
 # aws
 set AWS_PROFILE logicgate # or is it LG?
